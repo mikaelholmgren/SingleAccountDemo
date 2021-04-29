ba558bf117eec4a797a8d8807e7781694efcae1b
Mikael Holmgren <mikael.holmgren@mindinputtools.com>
2021-04-29 11:24:58 +0200

    Identity scaffolded

---
+++ SingleAccountDemo/Areas/Identity/Data/SingleAccountDemoUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SingleAccountDemo.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the SingleAccountDemoUser class
+    public class SingleAccountDemoUser : IdentityUser
+    {
+    }
+}
---
+++ SingleAccountDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SingleAccountDemo.Areas.Identity.Data;
+using SingleAccountDemo.Data;
+
+[assembly: HostingStartup(typeof(SingleAccountDemo.Areas.Identity.IdentityHostingStartup))]
+namespace SingleAccountDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SingleAccountDemoContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SingleAccountDemoContextConnection")));
+
+                services.AddDefaultIdentity<SingleAccountDemoUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SingleAccountDemoContext>();
+            });
+        }
+    }
+}
